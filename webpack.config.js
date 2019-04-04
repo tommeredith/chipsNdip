d56249f6eb8f607548765549fe9913ac554e28dd
Tom Meredith <tommeredith01@gmail.com>
2019-04-03 21:43:54 -0400

    all the package.json changes with babel to make it work

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
     template: "./src/index.html",
@@ -8,7 +7,6 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 });
 
 module.exports = {
-
     entry: "./src/index.js",
     output: {
         path: path.resolve("dist"),
